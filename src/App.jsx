aeae1110d32ae931c51d8f26240313bcefd449de
ukatzi <ytalkov@gmail.com>
2025-11-11 20:37:35 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ function App() {
   let [ch1, setCh1] = useState(0);
   let [ch2, setCh2] = useState(0);
   let [cur, setCur] = useState(0);
-  let [c2Promise, setC2Promise] = useState(null);
+  let [c2Data, setC2Data] = useState(0);
   let [isAnimationProceed, setIsAnimationProceed] = useState(false);
   let refName1 = useRef(null), refCount1 = useRef(null), refSrc1 = useRef(null), refOnClick1 = useRef(null);
   let refName2 = useRef(null), refCount2 = useRef(null), refSrc2 = useRef(null), refOnClick2 = useRef(null);
-  //let c2 = 0;
+  let c2 = 0;
   useEffect(() => {
     if(isAnimationProceed){
       let num = 0;
@@ -22,7 +22,7 @@ function App() {
       setTimeout(stopAnimation, 1000);
       function numIncreasing(){
         num++;
-        c2Promise.then(data =>{refCount2.current.innerHTML = Math.floor(data * num / 20)});
+        refCount2.current.innerHTML = Math.floor(c2Data * num / 20);
       }
       function stopAnimation(){
         clearInterval(myVar)
@@ -33,47 +33,37 @@ function App() {
         }
       }
     }
-  })
-  let ignore = false;
-  let first_tag = Math.floor(Math.random() * 100);
-  let second_tag = Math.floor(Math.random() * 100);
-  //let first_tag = 0;
-  //let second_tag = 1;
-
-  //alert(first_tag);
-  let name_tags = fetch("https://api.rule34.gg/getTags?raw=true&limit=1000&type=is_character")
-    .then(data => data.json());
+  });
   
-  let count_promise2 = name_tags.then(data => data["tags"][second_tag]["count"]);
 
   useEffect(() => {
-    let first_tag = Math.floor(Math.random() * 100);
-    let second_tag = Math.floor(Math.random() * 100);
-    //let first_tag = 0;
-    //let second_tag = 1;
+    let first_tag = Math.floor(Math.random() * 20);
+    let second_tag = Math.floor(Math.random() * 20);
 
-    //alert(first_tag);
     let name_tags = fetch("https://api.rule34.gg/getTags?raw=true&limit=1000&type=is_character")
       .then(data => data.json());
 
-      let tag_promise1 = name_tags.then(data => data["tags"][first_tag]["tag_name"]);
-      let tag_promise2 = name_tags.then(data => data["tags"][second_tag]["tag_name"])
+    let tag_promise1 = name_tags.then(data => data["tags"][first_tag]["tag_name"]);
+    let tag_promise2 = name_tags.then(data => data["tags"][second_tag]["tag_name"])
 
-      tag_promise1.then(data => {refName1.current.innerHTML = data;});
-      tag_promise2.then(data => {refName2.current.innerHTML = data;});
+    tag_promise1.then(data => {refName1.current.innerHTML = data;});
+    tag_promise2.then(data => {refName2.current.innerHTML = data;});
 
-      tag_promise1.then(data => {return fetch("https://api.rule34.gg/getRandomPosts?amount=1&tags=solo&tags=" + data)})
-        .then(data => data.json())
-        .then(data => {refSrc1.current.src = data["posts"][0]["preview"]["file"]});
-      tag_promise2.then(data => {return fetch("https://api.rule34.gg/getRandomPosts?amount=1&tags=solo&tags=" + data)})
-        .then(data => data.json())
-        .then(data => {refSrc2.current.src = data["posts"][0]["preview"]["file"]});
-    
+    tag_promise1.then(data => {return fetch("https://api.rule34.gg/getRandomPosts?amount=1&tags=solo&tags=sfw&tags=" + data)})
+      .then(data => data.json())
+      .then(data => {refSrc1.current.src = data["posts"][0]["preview"]["file"]});
+    tag_promise2.then(data => {return fetch("https://api.rule34.gg/getRandomPosts?amount=1&tags=solo&tags=sfw&tags=" + data)})
+      .then(data => data.json())
+      .then(data => {refSrc2.current.src = data["posts"][0]["preview"]["file"]});
+
       let count_promise1 = name_tags.then(data => data["tags"][first_tag]["count"]);
-    
+      let count_promise2 = name_tags.then(data => data["tags"][second_tag]["count"]);
       count_promise1.then(data => {refCount1.current.innerHTML = data});
-      count_promise1.then(data => {refCount2.current.innerHTML = "???"}); 
-      //count_promise2.then(data => {refCount2.current.innerHTML = data});
+      count_promise2.then(data => {
+        refCount2.current.innerHTML = "???";
+        c2 = data;
+        //alert(data);
+      }); 
 
       count_promise1.then(count1 => {
         count_promise2.then(count2 => {
@@ -91,14 +81,13 @@ function App() {
           }
         })
       });
-    return (() => {ignore = true;});
   }, [cur]);
   function onCorrect(){
     if(isAnimationProceed){
       return;
     }
     setIsAnimationProceed(true);
-    setC2Promise(count_promise2);
+    setC2Data(c2);
     setCount(count + 1); 
     setMaxCount(Math.max(maxCount, count + 1));
   }
@@ -107,7 +96,7 @@ function App() {
       return;
     }
     setIsAnimationProceed(true);
-    setC2Promise(count_promise2);
+    setC2Data(c2);
     setCount(0);
   }
   return (
